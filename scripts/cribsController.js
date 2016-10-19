b9bd87ceee63bbf6015ea9cee77e9e6a1a73d42f
JohnMorris4 <johnmorris@morrisje.com>
2016-10-19 12:49:07 -0400

    Initial Project Version

---
+++ scripts/cribsController.js
@@ -0,0 +1,22 @@
+angular
+    .module('ngCribs')
+    .controller('cribsController', function($scope, cribsFactory){
+
+   //TEST OBJECTS
+   $scope.cribs;
+    $scope.priceInfo = {
+        min: 0,
+        max: 10000000
+    }
+
+    $scope.newListing = {};
+    $scope.addCrib = function(newListing){
+        $scope.cribs.push(newListing);
+    }
+    cribsFactory.getCribs().success(function(data) {
+        $scope.cribs = data;
+    }).error(function(error){
+        console.log(error);
+    });
+
+});
